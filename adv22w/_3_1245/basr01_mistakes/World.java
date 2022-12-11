01f1a31395d8ef4d603e37521722e9570b485d80
Risqooo <74415563+Risqooo@users.noreply.github.com>
2022-12-11 16:41:46 +0100

    upraveny world

--- adv22w/_3_1245/basr01_mistakes/World.java
+++ adv22w/_3_1245/basr01_mistakes/World.java
@@ -86,111 +86,122 @@ public   class World
 //##############################################################################
 //\II== INSTANCE INITIALIZERS (CONSTRUCTORS) ===================================
 
-    /***************************************************************************
-     * Soukromý konstruktor definující jedinou instanci.
-     * Protože je soukromý, musí být definován, i když má prázdné tělo.
-     */
-    private World()
-    {
-        places = new ArrayList<>();
-        places.add(startPlace =
-                   new Place("Domeček",
-                       "Domeček, kde bydlí Karkulka",
-                       new String[] { "Les" },
-                       "_Bábovka", "_Víno", "#Stůl", "_Panenka"));
-        places.add(new Place("Les",
-                       "Les s jahodami, malinami a pramenem vody",
-                       new String[] { "Domeček", "Temný_les" },
-                       "_Maliny", "_Jahody", "#Studánka"));
-        places.add(new Place("Temný_les",
-                       "Temný_les s jeskyní a číhajícím vlkem",
-                       new String[] { "Les", "Jeskyně", "Chaloupka"},
-                       "#Vlk"));
-        places.add(new Place("Chaloupka",
-                       "Chaloupka, kde bydlí babička",
-                       new String[] { "Temný_les" },
-                       "#Postel", "#Stůl", "#Babička"));
-        places.add(new Place("Jeskyně",
-                       "Jeskyně, kde v zimě přespává medvěd",
-                       new String[] { "Temný_les" }
-                       /* nic */ ));
-        allPlaces = Collections.unmodifiableCollection(places);
-    }
+/***************************************************************************
+ * Soukromý konstruktor definující jedinou instanci.
+ * Protože je soukromý, musí být definován, i když má prázdné tělo.
+ */
+private World()
+{
+    places = new ArrayList<>();
+    places.add(startPlace =
+               new Place("Zeľuvarenije",
+                   "Obchod Černokňažníka",
+                   new String[] { "Varňa", "Dommosklo" },
+                   "#Pocestný", "_Pokladňa", "_Pohár", "#Pult", "_Stolička"));
+    places.add(new Place("Dommosklo",
+                   "Skleník s ingredienciami:" +
+                   "Zamiokulkas, Lopatkovec, Smíchovenec, Afrikule a Šachamašmak",
+                   new String[] {  "Varňa", "Zeľuvarenije" },
+                    "_Zamiokulkas", "_Lopatkovec",
+                    "_Smíchovenec", "_Afrikule", "_Šachamašmak"));
+    places.add(new Place("Varňa",
+                   "Varňa v ktorej je: " +
+                           "Špajza, Kotlík a Studňa",
+                   new String[] { "Dommosklo", "Zeľuvarenije",
+                           "Kotlík", "Špajza", "Studňa" },
+                   "_Varecha"));
+    places.add(new Place("Špajza",
+                   "Špajza v ktorej sa nachádza: " +
+                           "Dračígrc, Okomlok, Šalvia" +
+                           "Stromohnát a Rumbagule",
+                   new String[] { "Varňa" },
+                    "Dračígrc", "Okomlok", "Šalvia",
+                    "Stromohnát", "Rumbagule"));
+    places.add(new Place("Studňa",
+                   "Studňa, z ktorej musíš nabrať čistú vodu",
+                   new String[] { "Varňa" }
+                   /* nic */ ));
+    places.add(new Place("Kotlík",
+                    "Kotlík v ktorom uvaríš lektvar",
+                    new String[] { "Varňa" }
+                    /* nic */ ));
+    allPlaces = Collections.unmodifiableCollection(places);
+}
 
 
 
 //\IA== INSTANCE ABSTRACT METHODS ==============================================
 //\IG== INSTANCE GETTERS AND SETTERS ===========================================
 
-    /***************************************************************************
-     * Vrátí kolekci odkazů na všechny prostory vystupující ve hře.
-     *
-     * @return Kolekce odkazů na všechny prostory vystupující ve hře
-     */
-    @Override
-    public Collection<? extends IPlace> places()
-    {
-        return allPlaces;
-    }
+/***************************************************************************
+ * Vrátí kolekci odkazů na všechny prostory vystupující ve hře.
+ *
+ * @return Kolekce odkazů na všechny prostory vystupující ve hře
+ */
+@Override
+public Collection<? extends IPlace> places()
+{
+    return allPlaces;
+}
 
 
-    /***************************************************************************
-     * Vrátí odkaz na aktuální prostor,
-     * tj. na prostor, v němž se hráč pravé nachází.
-     *
-     * @return Prostor, v němž se hráč pravé nachází
-     */
-    @Override
-    public Place currentPlace()
-    {
-        return currentPlace;
-    }
+/***************************************************************************
+ * Vrátí odkaz na aktuální prostor,
+ * tj. na prostor, v němž se hráč pravé nachází.
+ *
+ * @return Prostor, v němž se hráč pravé nachází
+ */
+@Override
+public Place currentPlace()
+{
+    return currentPlace;
+}
 
 
-    /***************************************************************************
-     * Je li ve světě hry prostor se zadaným názvem, vrátí jej zabalený
-     * do objektu typu {@link Optional}, není-li tam, vrátí prázdný
-     * {@link Optional}.
-     *
-     * @param  name Název hledané prostoru
-     * @return Prostor zabalený do objektu {@link Optional},
-     *         nebo prázdný {@link Optional}.
-     */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @Override
-    public Optional<IPlace> oPlace(String name)
-    {
-        Optional oPlace = INamed.getO(name.toLowerCase(), places);
-        return oPlace;
-    }
+/***************************************************************************
+ * Je li ve světě hry prostor se zadaným názvem, vrátí jej zabalený
+ * do objektu typu {@link Optional}, není-li tam, vrátí prázdný
+ * {@link Optional}.
+ *
+ * @param  name Název hledané prostoru
+ * @return Prostor zabalený do objektu {@link Optional},
+ *         nebo prázdný {@link Optional}.
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+@Override
+public Optional<IPlace> oPlace(String name)
+{
+    Optional oPlace = INamed.getO(name.toLowerCase(), places);
+    return oPlace;
+}
 
 
-    /***************************************************************************
-     * Nastaví zadaný prosto jako aktuální, tj. jako prostor,
-     * v němž se aktuálně nachází hráč.
-     *
-     * @param destinationRoom Nastavovaný prostor
-     */
-    @Override
-    public void setCurrentPlace(IPlace destinationRoom)
-    {
-        currentPlace = (Place)destinationRoom;
-    }
+/***************************************************************************
+ * Nastaví zadaný prosto jako aktuální, tj. jako prostor,
+ * v němž se aktuálně nachází hráč.
+ *
+ * @param destinationRoom Nastavovaný prostor
+ */
+@Override
+public void setCurrentPlace(IPlace destinationRoom)
+{
+    currentPlace = (Place)destinationRoom;
+}
 
 
 
 //\IM== INSTANCE REMAINING NON-PRIVATE METHODS =================================
 
-    /***************************************************************************
-     * Inicializuje svět hry, tj. inicializuje propojení prostorů
-     * a jejich obsah a nastaví výchozí aktuální prostor.
-     */
-    @Override
-    public void initialize()
-    {
-        places.stream().forEach(Place::initialize);
-        currentPlace = startPlace;
-    }
+/***************************************************************************
+ * Inicializuje svět hry, tj. inicializuje propojení prostorů
+ * a jejich obsah a nastaví výchozí aktuální prostor.
+ */
+@Override
+public void initialize()
+{
+    places.stream().forEach(Place::initialize);
+    currentPlace = startPlace;
+}
 
 
 
